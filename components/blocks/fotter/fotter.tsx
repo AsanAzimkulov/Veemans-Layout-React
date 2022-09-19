49592fd240a5402b0c96f6bb622901ed5eb2b4f2
Asan Azimkulov <azimkulov40@gmail.com>
2022-09-19 14:20:28 +0600

    правки

--- components/blocks/fotter/fotter.tsx
+++ components/blocks/fotter/fotter.tsx
@@ -10,32 +10,42 @@ interface Props {
 const Fotter: FunctionComponent<Props> = ({ }) => {
   return (
     <div className={styles.container}>
-       <div className={styles.wrapper}>
+      <div className={styles.wrapper}>
 
-       		<div className={styles.topArea}>
-       			<img className={styles.logo} src="/logo.png" alt=""></img>
-             <div className={styles.topAreaButton}>
-               <Button1 variant="2">JOIN DISCORD</Button1>
-             </div>
-             <div className={styles.topAreaButton}>
-               <Button1 variant="2">FOLLOW US</Button1>
-             </div>
-       		</div>
+        <div className={styles.topArea}>
+          <img className={styles.logo} src="/logo.png" alt=""></img>
+          <div className={styles.topAreaButton + " " + styles.twitterButton}>
+            <Button1 variant="2">FOLLOW US</Button1>
+          </div>
+          <div className={styles.topAreaButton + " " + styles.discordButton}>
+            <Button1 variant="2">JOIN DISCORD</Button1>
+          </div>
+        </div>
 
-       		<img className={styles.line} src="/images/line.svg" />
+        <img className={styles.line} src="/images/line.svg" />
 
-       		<div className={styles.bottomArea}>
-            <div className={styles.bottomCell + ' ' + styles.bottomCellFirst}>Copyright © 2022 Veemans</div>
-            <div className={styles.bottomCell + ' ' + styles.bottomCellMiddle}>
-              <img src="/images/fotterSocial.svg" />
+        <div className={styles.bottomArea}>
+          <div className={styles.bottomCell + ' ' + styles.bottomCellFirst}>Copyright © 2022 Veemans</div>
+          <div className={styles.bottomCell + ' ' + styles.bottomCellMiddle}>
+            <div className={styles.block}>
+              <a><img src="/icons/ftelegram.svg" /></a>
+              <a href="#">              <img src="/icons/ftwitter.svg" />
+              </a>
+              <a href="#">              <img src="/icons/fmedium.svg" />
+              </a>
+
+              <a href="#">              <img src="/icons/fdiscord.svg" />
+              </a>
+
             </div>
-            <div className={styles.bottomCell + ' ' + styles.bottomCellLast}>
-              <a href="#">Cookies Policy</a>
-              <a href="#">Privacy & Policy</a>
-              <a href="#">Terms & Condition</a>
-            </div>
-       		</div>
-       </div>
+          </div>
+          <div className={styles.bottomCell + ' ' + styles.bottomCellLast}>
+            <a href="#">Cookies Policy</a>
+            <a href="#">Privacy & Policy</a>
+            <a href="#">Terms & Condition</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
