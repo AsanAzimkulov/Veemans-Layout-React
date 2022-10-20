c75d87204fd8bfbe03f15bbbbd98c85b63b04e66
Asan Azimkulov <azimkulov40@gmail.com>
2022-10-20 14:50:09 +0600

    правки

--- components/blocks/AcceptPopup/index.tsx
+++ components/blocks/AcceptPopup/index.tsx
@@ -24,7 +24,7 @@ const AcceptPopup: React.FC<TProps> = ({ onAccept, text, buttonText, href, linkT
       condition: visible
     })}>
       <button className={styles.close} onClick={() => setIsVisible(false)}>
-        <svg className={styles.closeIcon} width="10px" height="10px" viewBox="0 0 23 23" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"> <desc>Close</desc> <g stroke="none" strokeWidth="1" fill="#000000" fillRule="evenodd"> <rect transform="translate(11.313708, 11.313708) rotate(-45.000000) translate(-11.313708, -11.313708) " x="10.3137085" y="-3.6862915" width="2" height="30"></rect> <rect transform="translate(11.313708, 11.313708) rotate(-315.000000) translate(-11.313708, -11.313708) " x="10.3137085" y="-3.6862915" width="2" height="30"></rect> </g> </svg>
+        <svg className={styles.closeIcon} width="10px" height="10px" viewBox="0 0 23 23" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"> <desc>Close</desc> <g stroke="none" strokeWidth="1" fill="#5E1CEB" fillRule="evenodd"> <rect transform="translate(11.313708, 11.313708) rotate(-45.000000) translate(-11.313708, -11.313708) " x="10.3137085" y="-3.6862915" width="2" height="30"></rect> <rect transform="translate(11.313708, 11.313708) rotate(-315.000000) translate(-11.313708, -11.313708) " x="10.3137085" y="-3.6862915" width="2" height="30"></rect> </g> </svg>
       </button>
       <div className={styles.container}>
         <p className={styles.text}>{text}<a className={styles.link} href={href}>{' '}{linkText}</a></p>
